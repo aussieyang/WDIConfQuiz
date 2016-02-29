824e956160325a84c0215ec09b22ed92a0290eab
JoshSutcliffe <joshclement12@gmail.com>
2016-02-29 19:15:05 +1100

    working routes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,12 @@
 class DashboardController < ApplicationController
+
+  def index
+    # go to the index page that lists the quizzes
+    
+  end
+
+  def quiz
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+  get '/' => 'dashboard#index'
+  get '/dashboard' => 'dashboard#quiz'
+
   namespace :api do
     resources :questions
   end
-
-  root 'pages#home'
-
   
 end
